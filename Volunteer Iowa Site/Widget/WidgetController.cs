244b5b4adddaa5a3d51771b20ae79a119f63193d
Kevin W <kwickham@iastate.edu>
2013-03-18 16:53:01 -0500

    adding some widget files

---
+++ Volunteer Iowa Site/Widget/WidgetController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Volunteer_Iowa_Site.Widget
+{
+    public class WidgetController : Controller
+    {
+        //
+        // GET: /Widget/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
